9f55b9ab67c07a62684be3faf5bef84a89c08537
F. Martínez <122604561+fmartinez05@users.noreply.github.com>
2025-09-01 13:51:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,21 +2,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const headerPlaceholder = document.getElementById('main-header');
     const footerPlaceholder = document.getElementById('main-footer');
 
-    // Cargar Navbar
     if (headerPlaceholder) {
         fetch('/_navbar.html')
-            .then(response => response.text())
+            .then(response => {
+                if (response.ok) return response.text();
+                throw new Error('Navbar not found.');
+            })
             .then(data => {
                 headerPlaceholder.innerHTML = data;
-            });
+            })
+            .catch(error => console.error('Error loading navbar:', error));
     }
 
-    // Cargar Footer
     if (footerPlaceholder) {
         fetch('/_footer.html')
-            .then(response => response.text())
+            .then(response => {
+                if (response.ok) return response.text();
+                throw new Error('Footer not found.');
+            })
             .then(data => {
                 footerPlaceholder.innerHTML = data;
-            });
+            })
+            .catch(error => console.error('Error loading footer:', error));
     }
 });
